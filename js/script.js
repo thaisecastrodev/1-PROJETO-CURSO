f40df9890b8be578a8a4bf3e0e7c3703574c4f22
Thaise castro <thaisecastro.dev@gmail.com>
2026-05-18 14:31:17 -0300

    script

--- js/script.js
+++ js/script.js
@@ -1,36 +1,66 @@
+// Carregar resumos salvos ao abrir sistema
+window.onload = function () {
+
+  const dadosSalvos = localStorage.getItem("resumos");
+
+  if (dadosSalvos) {
+
+    const resumos = JSON.parse(dadosSalvos);
+
+    resumos.forEach(item => {
+      criarCard(item.texto, item.categoria);
+    });
+
+  }
+
+};
+
+// Função botão
 function gerarResumo() {
 
-  // Pegando texto
   const texto = document.getElementById("texto").value;
 
-  // Pegando categoria
   const categoria = document.getElementById("categoria").value;
 
-  // Área do histórico
-  const historico = document.getElementById("historico");
-
-  // Verificação simples
   if (texto.trim() === "") {
     alert("Digite algum conteúdo.");
     return;
   }
 
-  // Criando card
+  // Criar card visual
+  criarCard(texto, categoria);
+
+  // Pegar dados já salvos
+  let resumos = JSON.parse(localStorage.getItem("resumos")) || [];
+
+  // Adicionar novo resumo
+  resumos.push({
+    texto: texto,
+    categoria: categoria
+  });
+
+  // Salvar novamente
+  localStorage.setItem("resumos", JSON.stringify(resumos));
+
+  // Limpar campo
+  document.getElementById("texto").value = "";
+
+}
+
+// Função criar card
+function criarCard(texto, categoria) {
+
+  const historico = document.getElementById("historico");
+
   const card = document.createElement("div");
 
-  // Classe CSS
   card.classList.add("card");
 
-  // Conteúdo do card
   card.innerHTML = `
     <h3>${categoria}</h3>
     <p>${texto}</p>
   `;
 
-  // Adicionando card na tela
   historico.appendChild(card);
-
-  // Limpando textarea
-  document.getElementById("texto").value = "";
 
 }
